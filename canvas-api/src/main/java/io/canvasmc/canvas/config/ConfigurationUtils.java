3a069ae3368c234930d1316feb0086243df18e98
leir4iks <leir4iks@gmail.com>
2025-08-16 17:42:37 +0300

    cleanup (5?)

--- canvas-api/src/main/java/io/canvasmc/canvas/config/ConfigurationUtils.java
+++ canvas-api/src/main/java/io/canvasmc/canvas/config/ConfigurationUtils.java
@@ -2,64 +2,39 @@ package io.canvasmc.canvas.config;
 
 import io.canvasmc.canvas.config.annotation.Comment;
 import java.lang.reflect.Field;
-import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import java.util.WeakHashMap;
 import org.jetbrains.annotations.NotNull;
 
 public class ConfigurationUtils {
+    protected static final Map<String, Comment> COMMENTS = new LinkedHashMap<>();
+    protected static final Map<String, Field> FIELD_MAP = new LinkedHashMap<>();
 
-    private static final Map<Class<?>, ConfigData> CACHE = Collections.synchronizedMap(new WeakHashMap<>());
+    public static void extractKeys(Class<?> clazz) {
+        extractKeys(clazz, "");
+    }
 
-    private static class ConfigData {
-        final Map<String, Comment> comments = new LinkedHashMap<>();
-        final Map<String, Field> fieldMap = new LinkedHashMap<>();
-        final List<String> keys;
+    static @NotNull List<String> extractKeys(@NotNull Class<?> clazz, String prefix) {
+        List<String> keys = new LinkedList<>();
+        for (Field field : clazz.getDeclaredFields()) {
+            if (!java.lang.reflect.Modifier.isStatic(field.getModifiers())) {
+                String keyName = prefix.isEmpty() ? field.getName() : prefix + "." + field.getName();
+                keys.add(keyName);
 
-        ConfigData(Class<?> clazz) {
-            this.keys = extractKeysRecursive(clazz, "");
-        }
+                if (field.isAnnotationPresent(Comment.class)) {
+                    Comment comment = field.getAnnotation(Comment.class);
+                    COMMENTS.put(keyName, comment);
+                }
 
-        private List<String> extractKeysRecursive(@NotNull Class<?> clazz, String prefix) {
-            List<String> keyList = new LinkedList<>();
-            for (Field field : clazz.getDeclaredFields()) {
-                if (!java.lang.reflect.Modifier.isStatic(field.getModifiers())) {
-                    String keyName = prefix.isEmpty() ? field.getName() : prefix + "." + field.getName();
-                    keyList.add(keyName);
-
-                    if (field.isAnnotationPresent(Comment.class)) {
-                        Comment comment = field.getAnnotation(Comment.class);
-                        this.comments.put(keyName, comment);
-                    }
-
-                    field.setAccessible(true); // Ensure we can access the field
-                    this.fieldMap.put(keyName, field);
-                    if (field.getType().getEnclosingClass() == clazz) {
-                        keyList.addAll(extractKeysRecursive(field.getType(), keyName));
-                    }
+                FIELD_MAP.put(keyName, field);
+                if (field.getType().getEnclosingClass() == clazz) {
+                    keys.addAll(extractKeys(field.getType(), keyName));
                 }
             }
-            return Collections.unmodifiableList(keyList);
         }
+        return keys;
     }
 
-    private static ConfigData getConfigData(Class<?> clazz) {
-        return CACHE.computeIfAbsent(clazz, ConfigData::new);
-    }
-
-    public static List<String> extractKeys(Class<?> clazz) {
-        return getConfigData(clazz).keys;
-    }
-
-    // These methods are needed for other parts of the config system to access the cached data
-    static Map<String, Comment> getComments(Class<?> clazz) {
-        return getConfigData(clazz).comments;
-    }
-
-    static Map<String, Field> getFieldMap(Class<?> clazz) {
-        return getConfigData(clazz).fieldMap;
-    }
 }
