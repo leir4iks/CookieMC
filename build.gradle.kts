5b4d9e791772363b3ecd710c150f5960775f7204
leir4iks <118904014+leir4iks@users.noreply.github.com>
2025-08-16 16:13:45 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ import org.gradle.api.tasks.testing.logging.TestLogEvent
 plugins {
     java
     `maven-publish`
-    id("io.papermc.paperweight.patcher") version "2.2.2-SNAPSHOT"
+    id("io.papermc.paperweight.patcher") version "2.0.0-SNAPSHOT"
 }
 
 allprojects {
